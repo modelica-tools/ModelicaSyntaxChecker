c673f3aeaf53903b015b729677a5194bd4efcb9b
Elena Shmoylova <eshmoylova@users.noreply.github.com>
2023-11-17 15:44:02 -0500

    Added executables to support 3.6 syntax (specifically selective model extension). (#2)
    32 bit platforms are not supported anymore, so they were moved to Obsolete.
    
    Co-authored-by: U-SERVERS\eshmoylova <eshmoylova@W1164RD384.maplesoft.com>

--- Windows/checkMSL.bat
+++
@@ -1,6 +0,0 @@
-DEL log_Modelica.txt
-DEL log_ModelicaTest.txt
-moparser -e -r ..\..\..\..\trunk\Modelica > log_Modelica.txt
-moparser -e -r ..\..\..\..\trunk\ModelicaTest > log_ModelicaTest.txt
-
-
---
+++ Windows64/checkMSL.bat
@@ -0,0 +1,6 @@
+DEL log_Modelica.txt
+DEL log_ModelicaTest.txt
+moparser -e -r ..\..\..\..\trunk\Modelica > log_Modelica.txt
+moparser -e -r ..\..\..\..\trunk\ModelicaTest > log_ModelicaTest.txt
+
+
